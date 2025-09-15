95b3b8a8cf01ecbaa83ae717faa01ecd7f2042fd
peuanthinsan <heartpickle@gmail.com>
2025-09-15 07:38:22 +0700

    Reference GCS bucket secret in app config

--- backend/utils/gcs.js
+++ backend/utils/gcs.js
@@ -1,9 +1,17 @@
 import { Storage } from '@google-cloud/storage';
 
 const storage = new Storage();
-const bucket = storage.bucket(process.env.GCS_BUCKET);
+
+const getBucket = () => {
+  const bucketName = process.env.GCS_BUCKET;
+  if (!bucketName) {
+    throw new Error('GCS_BUCKET environment variable is not set');
+  }
+  return storage.bucket(bucketName);
+};
 
 export const uploadFile = async (file, destination) => {
+  const bucket = getBucket();
   const blob = bucket.file(destination);
   const stream = blob.createWriteStream({ resumable: false, contentType: file.mimetype });
   return new Promise((resolve, reject) => {
